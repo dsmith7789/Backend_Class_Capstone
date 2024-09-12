3e3a4a75e862527cae64e25e0e4dca5164742717
Dante Smith <dsmith7789@gmail.com>
2024-09-12 13:56:24 -0500

    add knex migration files

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -5,7 +5,7 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 // TODO: Add your routes here
 router.route("/:reviewId")
   .put(controller.update)
-  .delete(controller.delete)
+  .delete(controller.destroy)
   .all(methodNotAllowed);
 
 module.exports = router;
