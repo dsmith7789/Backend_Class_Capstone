0416137a84610f7ae1e4945c1a64caa0c76fbb10
Dante Smith <dsmith7789@gmail.com>
2024-09-12 15:11:16 -0500

    adding cors and error modules

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 
 // TODO: Add your code here
+app.use(cors());
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
